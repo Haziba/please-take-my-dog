abbc9f5016f0bad250248079de264adb895f9e77
Haziba <harry.boyes@gmail.com>
2017-07-31 12:29:00 +0100

    Load and display dogs

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -1,10 +1,43 @@
 var React = require('react');
 
 var Home = React.createClass({
+	componentWillMount: function(){
+		var that = this;
+
+		$.get("/api/dogs").then(function(dogs){
+			that.setState({dogs: dogs});
+		});
+	},
+
 	render: function() {
 		return (
-			<p>Home Page</p>
+			<div>
+				<p>Home Page</p>
+				{this._renderDogs()}
+			</div>
 		);
+	},
+
+	_renderDogs: function(){
+		let dogs = <div>Loading dogs...</div>;
+
+		if(this.state && this.state.dogs && this.state.dogs.length > 0){
+			dogs = 	<div>
+					{this.state.dogs.map(function(dog){ return <Dog details={dog} key={dog.name} /> })}
+				</div>;
+		}
+
+		return dogs;
+	}
+});
+
+var Dog = React.createClass({
+	render: function(){
+		return 	<div className="col-xs-6 col-sm-4 col-md-3">
+				<h3>{this.props.details.name}</h3>
+				<div><img className="col-xs-12" src={this.props.details.imageUrl} /></div>
+				<div>{this.props.details.breed}</div>
+			</div>;
 	}
 });
 
