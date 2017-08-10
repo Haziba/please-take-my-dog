ce2f851ed60ecaa642d3bb110871de7da0898f65
Haziba <harry.boyes@gmail.com>
2017-08-10 22:02:33 +0100

    Refactor DogTile

--- client/routes/dogs/View.js
+++ client/routes/dogs/View.js
@@ -1,27 +1,31 @@
 var React = require('react');
 
 var ViewDog = React.createClass({
+	getInitialState: function(){
+		return { loaded: false };
+	},
+
 	componentWillMount: function(){
 		var that = this;
 
-		$.get("/api/dog/" + this.props.params.id).then(function(details){
-			that.setState({details: details});
+		$.get("/api/dog/" + this.props.params.id).then(function(result){
+			that.setState({ loaded: true, details: result.data });
 		});
 	},
 
 	render: function() {
-		if(this.state && this.state.details){
-			return (
-				<div>
-					<p>{this.state.details.name}</p>
-					<p>Breed: {this.state.details.breed}</p>
-					<p>Size: {this.state.details.size}</p>
-					<img clasName="col-xs-12" src={this.state.details.imageurl} />
-				</div>
-			);
+		if(!this.state.loaded){
+			return <div><p>Loading {this.props.params.name}...</p></div>;
 		}
 
-		return <div><p>Loading {this.props.params.name}...</p></div>;
+		return (
+			<div>
+				<p>{this.state.details.name}</p>
+				<p>Breed: {this.state.details.breed}</p>
+				<p>Size: {this.state.details.size}</p>
+				<img clasName="col-xs-12" src={this.state.details.imageurl} />
+			</div>
+		);
 	}
 });
 
