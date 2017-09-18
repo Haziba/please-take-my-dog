a03e6e2e62bd28af6a27137dfe3140b58bd5b28b
Haziba <harry.boyes@gmail.com>
2017-09-18 12:42:37 +0100

    Use imagehelper for image on dog tiles

--- client/components/DogTile.js
+++ client/components/DogTile.js
@@ -1,13 +1,13 @@
 var React = require('react'),
 	Router = require('react-router'),
-	Image = require('cloudinary-react').Image;
+	ImageHelper = require('../helpers/ImageHelper.js');
 
 var DogTile = React.createClass({
 	render: function(){
 		return	<Router.Link to={"/dog/" + this.props.details.id + "/" + this.props.details.name}>
 				<div className="col-xs-6 col-sm-4 col-md-3">
 					<div className="dog-tile">
-						<div>{this._firstImage()}</div>
+						<div>{ImageHelper.FirstImage(this._images())}</div>
 						<h3 className="text-center">{this.props.details.name}</h3>
 					</div>
 				</div>
@@ -18,11 +18,6 @@ var DogTile = React.createClass({
 		if(!this.props.details.images || this.props.details.images.length < 1)
 			return false;
 		return JSON.parse(this.props.details.images);
-	},
-
-	_firstImage: function(){
-		let images = this._images();
-		return images ? <Image cloudName="haziba" publicId={images[0].public_id}></Image> : "";
 	}
 });
 
