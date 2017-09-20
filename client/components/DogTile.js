3bffcb4adb9a5450f6add8866407411ccc59fe93
Haziba <harry.boyes@gmail.com>
2017-09-20 23:28:33 +0100

    some image changes WIP

--- client/components/DogTile.js
+++ client/components/DogTile.js
@@ -11,7 +11,7 @@ var DogTile = React.createClass({
 		return	<div className="col-xs-6 col-sm-4 col-md-3">
 				<div className="dog-tile">
 					<Router.Link to={"/dog/" + this.props.details.id + "/" + this.props.details.name}>
-						<div>{ImageHelper.FirstImage(this.props.details.images)}</div>
+						<div>{ImageHelper.FirstImage(this.props.details.images, {tile_size: true})}</div>
 						<h3 className="text-center">{this.props.details.name}</h3>
 					</Router.Link>
 					{this.props.acceptTransfer ? <button className="btn btn-success" onClick={this._acceptTransfer} disabled={this.state.accepting}>Accept</button> : null}
--- client/helpers/ImageHelper.js
+++ client/helpers/ImageHelper.js
@@ -1,21 +1,44 @@
 var React = require('react'),
-  Image = require('cloudinary-react').Image;
+  Image = require('cloudinary-react').Image,
+  Transformation = require('cloudinary-react').Transformation;
 
 import ImageGallery from 'react-image-gallery';
 
 var ImageHelper = {
-  Image: function(image){
+  Image: function(image, opts){
+    console.log("wut");
     if(!image)
       return;
 
-    return <Image cloudName={'haziba'} publicId={image.public_id}></Image>;
+    let filters = {};
+
+    if(opts){
+      if(opts.gallery_size){
+        filters.width = 500;
+        filters.height = 400;
+      } else if(opts.tile_size){
+        filters.width = 320;
+        filters.height = 240;
+      }
+    }
+console.log("lol");
+    if(opts){
+      console.log(filters);
+      return <Image cloudName={'haziba'} publicId={image.public_id}>
+</Image>;
+      /*return <Image cloudName={'haziba'} publicId={image.public_id}>
+            <Transformation width={filters.width} height={filters.height}/>
+          </Image>;*/
+    } else {
+      return <Image cloudName={'haziba'} publicId={image.public_id} />;
+    }
   },
 
-  FirstImage: function(images){
+  FirstImage: function(images, opts){
     if(!images || images.length < 1)
       return;
 
-    return ImageHelper.Image(images[0]);
+    return ImageHelper.Image(images[0], opts);
   },
 
   Gallery: function(galleryImages){
@@ -38,7 +61,7 @@ var ImageHelper = {
       if(opts.gallery_size){
         filters += 'c_lpad,h_400,w_500';
       } else if(opts.tile_size){
-        filters += 'c_lpad,h_250,w_250';
+        filters += 'c_lpad,h_240,w_320';
       }
     }
 
