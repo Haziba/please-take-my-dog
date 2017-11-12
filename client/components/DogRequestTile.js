c798362fd9e40d7727a13c5e809c91a1babf1c37
Haziba <harry.boyes@gmail.com>
2017-11-12 11:09:28 +0000

    Use requestType on request tile

--- client/components/DogRequestTile.js
+++ client/components/DogRequestTile.js
@@ -8,11 +8,10 @@ var DogRequestTile = React.createClass({
 	},
 
 	render: function(){
-		console.log(this.props.request);
 		return	<div className="col-xs-6 col-sm-4 col-md-3">
 				<div className="dog-request-tile">
 					<Router.Link to={"/request/" + this.props.request.id}>
-						<h3 className="text-center">{this.props.carer.name} would like to have {this.props.dog.name}!</h3>
+						<h3 className="text-center">{this.props.carer.name} would like to {this.props.request.form.requestType} {this.props.dog.name}!</h3>
 						<div className="row">
 							<div className="col-xs-6">{ImageHelper.Image(this.props.carer.avatar)}</div>
 							<div className="col-xs-6">{ImageHelper.FirstImage(this.props.dog.images)}</div>
