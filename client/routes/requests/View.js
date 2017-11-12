cca098966b27dc1bb234a4410768b472c8e09b14
Haziba <harry.boyes@gmail.com>
2017-11-12 11:11:18 +0000

    Show form answers on requests view page

--- client/routes/requests/View.js
+++ client/routes/requests/View.js
@@ -10,7 +10,7 @@ var ViewRequest = React.createClass({
 	componentWillMount: function(){
 		var that = this;
 
-	   $.get("/request/" + this.props.params.id).then(function(result){
+	   $.get("/api/request/" + this.props.params.id).then(function(result){
 			let admin;
 			if(that.state.self){
 				admin = that.state.self.id == result.data.request.carerid;
@@ -24,7 +24,7 @@ var ViewRequest = React.createClass({
 				admin = that.state.dog.carerid == result.carer.id;
 			}
 			that.setState({ self: result.carer, admin: admin });
-		});*/
+		});
 	},
 
 	render: function() {
@@ -32,11 +32,46 @@ var ViewRequest = React.createClass({
 			return <div><p>Loading request...</p></div>;
 		}
 
+		let formAnswers =
+						<div className="col-xs-12 text-center">
+							<button className="btn btn-primary" onClick={this._handleViewFormAnswers}><span>View Form Answers</span></button>
+						</div>;
+
+		if(this.state.viewFormAnswers){
+			formAnswers = <div className="col-xs-12">
+					<dl>
+					{Object.keys(this.state.request.form.questions).map(key =>
+						<div>
+							<dt>{this.state.request.form.questions[key].q}</dt>
+							<dd>{this.state.request.form.questions[key].a}</dd>
+						</div>)}
+					</dl>
+				</div>
+		}
+
 		return (
-		<div>
-      Request! {this.state.request}
-		</div>
+		  <div className="row">
+        <div className="col-xs-12 text-center">
+          <h2>{this.state.carer.name} would like to {this.state.request.form.requestType} {this.state.dog.name}</h2>
+        </div>
+
+        <div className="col-xs-12 col-md-4 col-md-offset-1">
+          {ImageHelper.Image(this.state.carer.avatar, {request_size: true})}
+        </div>
+
+        <div className="col-xs-12 col-md-4 col-md-offset-2">
+          {ImageHelper.FirstImage(this.state.dog.images, {request_size: true})}
+        </div>
+
+				{formAnswers}
+		  </div>
 		);
+	},
+
+	_handleViewFormAnswers: function(){
+		console.log("Handle view form answers");
+
+		this.setState({viewFormAnswers: true});
 	}
 });
 
