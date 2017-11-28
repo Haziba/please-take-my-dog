aebcc53fe6a2f3f773f23fd7a2f606b3231079ef
Haziba <harry.boyes@gmail.com>
2017-11-28 21:22:56 +0000

    Set up eventBus pubsub

--- server/entities/eventBus.js
+++ server/entities/eventBus.js
@@ -1,4 +1,20 @@
 //todo: Build event bus
 module.exports = {
-  fu: "bar"
+  sub: (entityType, event, callback) => {
+    if(!this._subs)
+      this._subs = {};
+    if(!this._subs[entityType])
+      this._subs[entityType] = {};
+    if(!this._subs[entityType][event])
+      this._subs[entityType][event] = [];
+    this._subs[entityType][event].push(callback);
+  },
+
+  pub: (entityType, entity, event, data) => {
+    if(this._subs && this._subs[entityType] && this._subs[entityType][event]){
+      for(let sub of this._subs[entityType][event]){
+        sub(entity, data);
+      }
+    }
+  }
 };
