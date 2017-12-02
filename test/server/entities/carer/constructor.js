3683185c84e7f8f6f6335df2a78915b43f70be73
Haziba <harry.boyes@gmail.com>
2017-12-02 23:25:36 +0000

    Add tests for new Carer()

---
+++ test/server/entities/carer/constructor.js
@@ -0,0 +1,46 @@
+require('../../../mock.js');
+const Carer = require('../../../../src/server/entities/carer.js');
+
+const assert = require('assert');
+const passwordHash = require('password-hash');
+
+describe('carer', function() {
+  describe('Create', function() {
+    describe('ValidCall', function() {
+      const name = 'test name';
+      const email = 'test email';
+      const _password = 'test password';
+      const _authToken = 'test authToken';
+      const avatar = 'test avatar';
+      const dogs = 'test dogs';
+      
+      const carer = new Carer({ data: {
+          name, email, _password, _authToken, avatar, dogs
+      }});
+      
+      it('should set the name', () => {
+        carer.name = name;
+      });
+      
+      it('should set the email', () => {
+        carer.email = email;
+      });
+      
+      it('should set the _password', () => {
+        carer._password = _password;
+      });
+      
+      it('should set the _authToken', () => {
+        carer._authToken = _authToken;
+      });
+      
+      it('should set the avatar', () => {
+        carer.avatar = avatar;
+      });
+      
+      it('should set the dogs', () => {
+        carer.dogs = dogs;
+      });
+    });
+  });
+});
